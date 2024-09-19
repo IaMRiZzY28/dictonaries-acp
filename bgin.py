a0c1b23ce7cf78a1b82a26ce3cb5bc06be187e54
IaMRiZzY28 <28ragavn@gmail.com>
2024-09-19 17:20:37 +0530

    g

---
+++ bgin.py
@@ -0,0 +1,9 @@
+
+boysag = {"name": "ragav", "age": 11}
+print(boysag["name"])
+boysag["age"] = 31
+boysag["city"] = "New York"
+del boysag["age"]
+print(boysag)
+squares = {x: x**2 for x in range(5)}
+print(squares)
